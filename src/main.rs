c14d323e3ea4be78505bcd0e68675b9be858e62c
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-04-23 16:02:47 +0000

    static storing the version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[allow(unused_variables)]
+fn read_version(transaction_hex: &str)->u32 {
+    return 1;
+}
+
+#[allow(unused_variables)]
 fn main() {
-    println!("Hello, world!");
+    let version = read_version("02000000000101b0e1f89573c0d5313848c962b4af6df0ce6683776f88dce5a8052847aea05aac0000000000fdffffff0273170d8f00000000160014bb6380980fca48fce5d5a582452ebe9cd1396da600e1f505000000001600146a5bd823854e872619f226a5fbe81ae6d4c1435f02473044022038260a253503ce5d74a941076d6399544d04a6dfdcf9249f07a090811d7d7c4b022064e1befcd5529acc0e1a3f89d4bdf33db1b60d33b0dadd41435453c180ebdf28012102d514ac4d4be3af4525b1e4742f8d5eb0b0400366714c72ef831f0ac9722c2de92f010000");
+    println!("version: {}!", version);
 }
