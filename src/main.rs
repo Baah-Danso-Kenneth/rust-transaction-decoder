7ff3b2d027b5c2692e1c7e209659c20698167874
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-05-06 03:22:22 +0000

    Arrays stacks and pointers

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #[allow(unused_variables)]
 fn read_version(transaction_hex: &str)->u32 {
     let transaction_bytes = hex::decode(transaction_hex).unwrap();
-    let version_bytes = &transaction_bytes[0..4];
-    print!("veresion bytes: {:?}", version_bytes);
-    return 1;
+    let version_bytes = (&transaction_bytes[0..4]).try_into().unwrap();
+    u32::from_le_bytes(version_bytes)
+
+    
 }
 
 #[allow(unused_variables)]
